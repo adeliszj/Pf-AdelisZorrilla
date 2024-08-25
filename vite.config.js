8508723c6b99c0463b863a2547c0ccd43196499a
Adelis Zorrilla <adeliszj@gmail.com>
2024-08-24 23:38:26 -0400

    Update vite config

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                contact: resolve(__dirname, 'contact.html'),
+                'best-sellers': resolve(__dirname, 'best-sellers.html'),
+            },
+        },
+    },
+})
