18cd734cde59e5c06f1622490f60bac82435c46c
Adelis Zorrilla <adeliszj@gmail.com>
2024-09-30 20:15:12 -0300

    Added images of various and seasonal flowers.

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
-// vite.config.js
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import fs from 'fs'
 
 export default defineConfig({
     build: {
         rollupOptions: {
             input: {
+                // Include the root-level index.html
                 main: resolve(__dirname, 'index.html'),
-                contact: resolve(__dirname, 'contact.html'),
-                'best-sellers': resolve(__dirname, 'best-sellers.html'),
+                // Automatically include all .html files in ./pages directory
+                ...Object.fromEntries(
+                    fs.readdirSync(resolve(__dirname, 'pages'))
+                        .filter(file => file.endsWith('.html')) // Filter for .html files
+                        .map(file => [file.replace('.html', ''), resolve(__dirname, 'pages', file)])
+                ),
             },
         },
     },
